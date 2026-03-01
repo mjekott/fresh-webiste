a4e248c8659014d1703372fda8262c273447d1f2
jonathan <kent93226@gmail.com>
2026-03-01 19:53:13 +0100

    feat(home): implement hero section UI

--- app/(main)/_components/Glassmorphism.tsx
+++ app/(main)/_components/Glassmorphism.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from "react";
 
 const Glassmorphism = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="w-full bg-white/30 backdrop-blur-md border border-white/20 p-4 shadow-md">
+    <div className="w-full bg-white/30 backdrop-blur-md border rounded-full border-white/20 p-2.5 shadow-md">
       {children}
     </div>
   );
--- components/index.tsx
+++ components/index.tsx
@@ -3,4 +3,5 @@ import leafLight from "@/public/images/leaf-light.png";
 import leafDark from "@/public/images/leaf-dark.png";
 import onion from "@/public/images/onion.png";
 import flower from "@/public/images/flower.png";
-export { img40, leafLight, leafDark, onion, flower };
+import smileEmoji from "@/public/images/ya.png";
+export { img40, leafLight, leafDark, onion, flower, smileEmoji };
